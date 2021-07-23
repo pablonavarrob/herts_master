8147c94055193a1a5ceaabe3f2d9e48fc143b899
Pablo Navarro <pablonavarrobarrachina@gmail.com>
2021-07-23 08:40:33 +0200

    CNN working: 0.95 test accuracy

--- CNN_functions.py
+++ CNN_functions.py
@@ -12,47 +12,49 @@ def normalize_image_data(image_data):
 
     if isinstance(image_data, pd.DataFrame):
         # Convert to array:
-        image_data = image_data.to_numpy().reshape(len(image_data), 110, 110)
+        image_data = image_data.to_numpy()
 
     else:
         pass
 
-    return (image_data/255).astype('float32')
+    return (image_data/255).reshape(len(image_data), 110, 110, 1).astype('float32')
 
 def build_model_CNN_v1(INPUT_SHAPE):
     """ Constructs the object model for the convolutional neural network
-    through sequential layers. """
+    through sequential layers.
+
+    The main architecture for the preliminary version will be based on a
+    standard construct from the Deep Learning book: 3 convolutional layers
+    and a fully connected layer at the end
+
+    """
 
     model = Sequential()
 
-    # The main architecture for the preliminary version will be based on a
-    # standard construct from the Deep Learning book: 3 convolutional layers
-    # and a fully connected layer at the end
-
     # Block 1
-    model.add(Conv2D(3, 3), input_shape=INPUT_SHAPE, activation='relu')
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(rate=0.25))
-
-    # Block 2
-    model.add(Conv2D(4, 4), activation='relu')
+    model.add(Conv2D(5, (3, 3), activation='relu', input_shape=INPUT_SHAPE))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(rate=0.3))
 
-    # Block 3
-    model.add(Conv2D(6, 6), activation='relu')
+    # Block 2
+    model.add(Conv2D(10, (4, 4), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(rate=0.35))
 
-    # Block 4
-    model.add(Conv2D(8, 8), activation='relu')
+    # Block 3
+    model.add(Conv2D(10, (6, 6), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(rate=0.4))
 
+    # Block 4
+    model.add(Conv2D(15, (8, 8), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(rate=0.45))
+
     # Dense layer
     model.add(Flatten())
-    model.add(Dense(64, activation='relu'))
-    model.add(Dropout(rate=0.3))
+    model.add(Dense(256, activation='relu'))
+    model.add(Dropout(rate=0.5))
     model.add(Dense(1, activation='sigmoid')) # Classification layer
 
     return model
