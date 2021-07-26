d82f25be76f3ef8a9f0f841d20309fd41e061a78
Pablo Navarro <pablonavarrobarrachina@gmail.com>
2021-07-26 17:29:46 +0200

    improved augmentation with further filters

--- augmentation_functions.py
+++ augmentation_functions.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from tensorflow.image import random_contrast, random_brightness
 
 
 def augmentation(image_data, label_data):
@@ -21,7 +22,7 @@ def augmentation(image_data, label_data):
         })
 
     augmented_labels = []  # Create empty list, append dictionaries
-    augmented = np.zeros((len(image_data)*5, 110, 110))
+    augmented = np.zeros((len(image_data)*11, 110, 110))
     i = 0
     j = 0
     for img in image_data:
@@ -37,7 +38,19 @@ def augmentation(image_data, label_data):
         append_labels(j, "flipud")
         augmented[i+4] = np.fliplr(img)  # Mirror left to right
         append_labels(j, "fliplr")
-        i += 5
+        augmented[i+5] = contrast_randomize(img)  # Contrast
+        append_labels(j, "contrast")
+        augmented[i+6] = exposure_randomize(img)  # Exposure
+        append_labels(j, "exposure")
+        augmented[i+7] = np.fliplr(contrast_randomize(img))  # Contrast + fliplr
+        append_labels(j, "contrast_fliplr")
+        augmented[i+8] = np.fliplr(exposure_randomize(img))  # Exposure + fliplr
+        append_labels(j, "exposure_fliplr")
+        augmented[i+9] = np.flipud(contrast_randomize(img))  # Contrast + flipud
+        append_labels(j, "contrast_flipud")
+        augmented[i+10] = np.flipud(exposure_randomize(img))  # Exposure + flipud
+        append_labels(j, "exposure_flipud")
+        i += 11
         j += 1
 
     return augmented, pd.DataFrame(augmented_labels)
@@ -55,3 +68,13 @@ def rotate_270(img):
     concatenates three np.rot90 statemenets. '''
 
     return np.rot90(np.rot90(np.rot90(img)))
+
+def contrast_randomize(img):
+    ''' Randomly increases/decreases the contrast of the input image. '''
+
+    return tf.reshape(random_contrast(img, lower=0.5, upper=1.5), (110, 110)).numpy()
+
+def exposure_randomize(img):
+    ''' Randomly increases/decreases the exposure of the input image. '''
+
+    return tf.reshape(random_brightness(img, 0.35), (110, 110)).numpy()
