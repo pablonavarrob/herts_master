fc86ec4afd1b0a7a2ee54ac63d9e93d8ca372db3
Pablo Navarro <pablonavarrobarrachina@gmail.com>
2021-08-10 08:42:08 +0200

    keep working on the evaluation..

--- classical_algorithm_models.py
+++ classical_algorithm_models.py
@@ -12,7 +12,7 @@ from sklearn.model_selection import ParameterGrid
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-## Same data loading pipeline
+## Same data loading pipeline ##################################################
 # Import data without the corrupted images
 img_data, lbl_data = load_cleaned_volcano_data()
 print('Data loaded')
@@ -31,34 +31,8 @@ y_train = y_train.to_numpy()
 y_test = y_test.to_numpy()
 print('Train/test split done')
 
-### Class balancing
-# # Adjust weight for all classes automatically inversely proportional to class
-# # frequency
-# weights = get_weight(lbl_data)
-# balanced_weights = {
-#     0: weights[0],
-#     1: weights[1]
-# }
 
-### Try with SVC (Support Vector Classification)
-# Do a parameter space exploration
-svc_param_dict = {
-    'kernel': ['rbf', 'poly'],
-    'C': [0.1, 1, 10, 100],
-    'gamma': [100, 10, 10, 0.1, 0.01],
-    'class_weight': [None, 'balanced']
-}
-
-# Create the parameter grid
-param_grid_svc = ParameterGrid(svc_param_dict)
-
-# Run the function for all the created dicts - store the results
-for params in param_grid:
-    # Train, get results from trainig, run test -> save all that info in a dict?
-    params = param_grid[10]
-    svc_classifier = SVC(verbose=1, n_jobs=-1, **params).fit(X_test, y_test)
-
-### Try with random forest classifier
+### Try with random forest classifier ##########################################
 # Do a parameters space exploration
 rfc_param_dit = {
     'criterion': ['gini', 'entropy'],
@@ -77,10 +51,11 @@ for i in range(len(param_grid_rfc)):
         verbose=1,
         **param_grid_rfc[i]
     )
+
     clf.fit(X_train, y_train)
     y_pred = clf.predict(X_test)
     acc = accuracy_score(y_test, y_pred)
     results.append({
         'idx': i,
         'accuracy': acc
-    })
+        })
