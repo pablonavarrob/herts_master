72420b5987a5b78e4c4ee82f47ff2c5c3de9de7a
Pablo Navarro <pablonavarrobarrachina@gmail.com>
2021-08-17 15:10:32 +0200

    creating plots for written thesis

--- cleaning.py
+++ cleaning.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from data_loading_functions import load_raw_volcano_data
 from cleaning_functions import remove_corrupted_images
+import matplotlib.pyplot as plt
 
 SAVE_DATA_TO_FILE = False
 
@@ -12,6 +13,20 @@ total_lbl = total_lbl.reset_index(drop=True)
 image_data_non_corrupted, idx_corrupted_image = remove_corrupted_images(
         total_img)
 train_labels_non_corrupted = total_lbl.drop(idx_corrupted_image)
+
+# Plot some of the corrupted images
+corrupted = total_img.iloc[idx_corrupted_image]
+corrupted = corrupted.to_numpy().reshape(378, 110, 110)
+fig, ax = plt.subplots(2, 3, figsize=[15, 10], tight_layout=True)
+for i in range(3):
+    ax[0, i].imshow(corrupted[i + 235], cmap='gray')
+    ax[0, i].set_xticklabels([])
+    ax[0, i].set_yticklabels([])
+    ax[1, i].imshow(corrupted[i + 200], cmap='gray')
+    ax[1, i].set_xticklabels([])
+    ax[1, i].set_yticklabels([])
+plt.savefig('figures/corruptedvolcanoes.png', dpi=150)
+
 
 if SAVE_DATA_TO_FILE:
     image_data_non_corrupted.to_csv('image_data_non_corrupted.csv', index=False)
--- plots_debug.py
+++ plots_debug.py
@@ -27,6 +27,28 @@ ax[1].imshow(np.flipud(a))
 ax[1].title.set_text("Mirrored image: vertical axis")
 plt.show()
 
+### DATA AUGMENTATION PLOTS #####
+a = train_images.iloc[0].to_numpy().reshape(110, 110)
+fig, ax = plt.subplots(2, 2, figsize=[10, 10], tight_layout=True)
+fig.suptitle("Image augmentation: rotation", fontsize=20)
+ax[0, 0].imshow(a)
+ax[0, 0].title.set_text("Original image")
+ax[0, 1].imshow(np.rot90(a))
+ax[0, 1].title.set_text("90 deg. rotation")
+ax[1, 0].imshow(rotate_180(a))
+ax[1, 0].title.set_text("180 deg. rotation")
+ax[1, 1].imshow(rotate_270(a))
+ax[1, 1].title.set_text("270 deg. rotation")
+plt.show()
+
+fig, ax = plt.subplots(2, 1, figsize=[10, 10], tight_layout=True)
+fig.suptitle("Image augmentation: mirroring", fontsize=20)
+ax[0].imshow(np.fliplr(a))
+ax[0].title.set_text("Mirrored image: horizontal axis")
+ax[1].imshow(np.flipud(a))
+ax[1].title.set_text("Mirrored image: vertical axis")
+plt.show()
+
 ### OVERVIEW PLOTS #####
 # Study the balancing of the data set
 total_img, total_lbl = load_raw_volcano_data(False)
